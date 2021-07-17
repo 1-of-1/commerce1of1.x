7de8b6fcaed02cfe2bc9bb44d1c80dd6e7a9fb54
aooluoch <aooluoch@amazon.com>
2021-07-17 16:16:05 -0400

    initial commit

---
+++ src/components/Nav/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+
+import logo from '../../assets/oneofone.jpg'
+import useStyles from './styles';
+
+const Navbar = () => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <Typography varient="h6" className={classes.title} color="primary">
+              <img src={logo} alt="1of1" height="25px" className={classes.image} />
+              1of1.js
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.grow} />
+              <IconButton aria-label="Show cart items" color="inherit">
+                  <Badge badgeContent={2} color="secondary">
+                      <ShoppingCart />
+                  </Badge>
+              </IconButton>
+        </Toolbar>
+      </AppBar>
+    </>
+  )
+}
+
+export default Navbar;
