1f6b5d914e943366b4ba1face4b405bc335d9f70
aooluoch <aooluoch@amazon.com>
2021-07-29 20:58:08 -0400

    cart changes

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -20,8 +20,8 @@ const Products = ({ products, onAddToCart }) => {
       <Grid container justifyContent="center" spacing={4}>
           {products.map((product) => (
               <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                  <Product product={product} onAddToCart={onAddToCart} />
-                  </Grid>
+                <Product product={product} onAddToCart={onAddToCart} />
+              </Grid>
           ))}
       </Grid>
   </main> 
